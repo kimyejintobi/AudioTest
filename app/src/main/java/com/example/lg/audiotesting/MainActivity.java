e2ec84eae921f4847695bddc5e6576377f8d287c
kimyejintobi <7602kyj@naver.com>
2017-06-19 20:29:04 +0900

    #first

---
+++ app/src/main/java/com/example/lg/audiotesting/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.lg.audiotesting;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    Switch switchMusic;
+    MediaPlayer musicPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        switchMusic=(Switch)findViewById(R.id.switch_music);
+        musicPlayer= MediaPlayer.create(this,R.raw.handonme);
+        switchMusic.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+                if (switchMusic.isChecked())
+                    musicPlayer.start();
+                else
+                    musicPlayer.pause();
+            }
+        });
+    }
+        @Override
+                protected  void onStop(){
+                super.onStop();
+                musicPlayer.stop();
+    }
+}
